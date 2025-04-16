846c65bcaedde2d7f803482a2a183284dd61fbfb
Tom Zhang <tomzhang.canada888@gmail.com>
2025-04-16 00:45:26 -0400

    last update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,10 @@ const Hero = () => {
                 <h1 className="text-4xl text-white font-bold">Hi There!</h1>
                 <p className="text-xl md:text-2xl text-white mt-4">Welcome to Tom's World</p>
             </div>
+            {/* Add the Last Updated text using Tailwind */}
+            <span className="absolute bottom-4 right-4 text-xs text-gray-300">
+                Last updated 2025-01-21
+            </span>
         </section>
     )
 }
