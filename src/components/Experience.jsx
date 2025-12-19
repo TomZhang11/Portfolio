2a19636c940186fc400df9af33dd979b23f9b5bf
Tom Zhang <tomzhang.canada888@gmail.com>
2025-12-19 13:15:45 -0500

    delete transcript

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,16 +34,6 @@ const Experience = () => {
                 >
                     View Full Resume
                 </a>
-
-                <a
-                    href="/transcript.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-flex items-center px-6 py-3 bg-white rounded-lg shadow-md hover:shadow-lg 
-                        transition-all duration-300 hover:scale-105 hover:bg-[#a8a4b5] text-gray-600 ml-4"
-                >
-                    View Transcript
-                </a>
             </div>
         </section>
     );
